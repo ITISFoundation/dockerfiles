4fae421f625e6621b88867ad60f8be466c6c150a
Andrei Neagu <neagu@itis.swiss>
2020-06-22 12:13:56 +0200

    avoids issues with wrongly named registries

--- ci/github/action-build-and-push/entrypoint.bash
+++ ci/github/action-build-and-push/entrypoint.bash
@@ -18,15 +18,15 @@ python3 /code/print_as_header.py "Switching to project directory '$INPUT_TARGET_
 cd $INPUT_TARGET_PROJECT_PATH_IN_GIT_REPO
 
 python3 /code/print_as_header.py "Trying to pull existing image to enable caching"
-DOCKER_REGISTRY=$TARGET_REGISTRY_NAME make github-ci-pull  || true
+DOCKER_REGISTRY=$INPUT_TARGET_REGISTRY_NAME make github-ci-pull  || true
 
 python3 /code/print_as_header.py "Building image"
-DOCKER_REGISTRY=$TARGET_REGISTRY_NAME make github-ci-build
+DOCKER_REGISTRY=$INPUT_TARGET_REGISTRY_NAME make github-ci-build
 
 python3 /code/print_as_header.py "Running tests"
-DOCKER_REGISTRY=$TARGET_REGISTRY_NAME make github-ci-tests
+DOCKER_REGISTRY=$INPUT_TARGET_REGISTRY_NAME make github-ci-tests
 
 python3 /code/print_as_header.py "Pushing image"
-DOCKER_REGISTRY=$TARGET_REGISTRY_NAME make github-ci-push
+DOCKER_REGISTRY=$INPUT_TARGET_REGISTRY_NAME make github-ci-push
 
 python3 /code/print_as_header.py "Completed without errors"
