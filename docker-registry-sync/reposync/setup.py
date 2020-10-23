c86b3c06afa6e73d9796d89b9b23142299775d32
Andrei Neagu <neagu@itis.swiss>
2020-10-23 16:03:29 +0200

    added new entrypoint

--- docker-registry-sync/reposync/setup.py
+++ docker-registry-sync/reposync/setup.py
@@ -21,9 +21,16 @@ setup(
     name="reposync",
     version="2.0.0",
     packages=find_packages(where="src"),
-    package_dir={"": "src",},
+    package_dir={
+        "": "src",
+    },
     include_package_data=True,
-    entry_points={"console_scripts": ["run-reposync=reposync.entrypoint:main",]},
+    entry_points={
+        "console_scripts": [
+            "run-reposync=reposync.entrypoint:main",
+            "generate-table=reposync.services_table:main",
+        ]
+    },
     python_requires=">=3.7",
     install_requires=install_requirements,
     tests_require=test_requirements,
