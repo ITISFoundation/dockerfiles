90149325c1875ccebbd971867f296a41b978036d
Andrei Neagu <neagu@itis.swiss>
2025-02-25 15:47:50 +0100

    refactor

--- docker-registry-sync/reposync/setup.py
+++ docker-registry-sync/reposync/setup.py
@@ -1,6 +1,5 @@
 import os
 import re
-import sys
 from pathlib import Path
 
 from setuptools import find_packages, setup
@@ -24,7 +23,7 @@ setup(
     package_dir={"": "src",},
     include_package_data=True,
     entry_points={"console_scripts": ["run-reposync=reposync.main:main",]},
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=install_requirements,
     tests_require=test_requirements,
     setup_requires=["setuptools_scm"],
