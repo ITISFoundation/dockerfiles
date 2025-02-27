74bff33001cf4c7994a53b92956dba9ae38b1f6a
Andrei Neagu <neagu@itis.swiss>
2025-02-27 11:11:53 +0100

    bumped coverage

--- docker-registry-sync/reposync/tests/test_cli.py
+++ docker-registry-sync/reposync/tests/test_cli.py
@@ -43,19 +43,19 @@ def test_cyclic_dependency_conf(
 
 
 def test_valid_configuration_ok(
-    get_config_file: Callable[[str], str], caplog_debug: pytest.LogCaptureFixture
+    mock_crane: None,
+    get_config_file: Callable[[str], str],
+    caplog_debug: pytest.LogCaptureFixture,
 ):
-    result = runner.invoke(
-        cli.app, [get_config_file("min_valid_conf.yaml"), "--debug", "--verify-only"]
-    )
+    result = runner.invoke(cli.app, [get_config_file("min_valid_conf.yaml"), "--debug"])
     assert result.exit_code == os.EX_OK, _format_cli_error(result)
-    assert "Configuration is OK, closing gracefully." in caplog_debug.text
+    assert "Image sync took" in caplog_debug.text
 
 
 def test_multi_stage_ids_conf(get_config_file: Callable[[str], str]):
     result = runner.invoke(
         cli.app,
-        [get_config_file("multi_stage_ids_conf.yaml"), "--debug", "--verify-only"],
+        [get_config_file("multi_stage_ids_conf.yaml"), "--debug"],
     )
     assert result.exit_code != os.EX_OK, _format_cli_error(result)
     assert "stages[#].id must be unique" in f"{result}"
@@ -64,7 +64,7 @@ def test_multi_stage_ids_conf(get_config_file: Callable[[str], str]):
 def test_no_such_stage_conf(get_config_file: Callable[[str], str]):
     result = runner.invoke(
         cli.app,
-        [get_config_file("no_such_stage_conf.yaml"), "--debug", "--verify-only"],
+        [get_config_file("no_such_stage_conf.yaml"), "--debug"],
     )
     assert result.exit_code != os.EX_OK, _format_cli_error(result)
     assert "stage.depends_on entry" in f"{result}"
@@ -73,11 +73,7 @@ def test_no_such_stage_conf(get_config_file: Callable[[str], str]):
 def test_stages_self_dependency_conf(get_config_file: Callable[[str], str]):
     result = runner.invoke(
         cli.app,
-        [
-            get_config_file("stages_self_dependency_conf.yaml"),
-            "--debug",
-            "--verify-only",
-        ],
+        [get_config_file("stages_self_dependency_conf.yaml"), "--debug"],
     )
     assert result.exit_code != os.EX_OK, _format_cli_error(result)
     assert "stage cannot depend on itself" in f"{result}"
